dcc0007ddc2e7ed519d54950e2a73bf0925166e8
Juvikk <alex.kondor2002@mail.ru>
2024-07-17 02:51:56 +0400

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = ('бобр',1721,'штандарт',True)
+
+#immutable_var [0]= 'кваказябра'
+##значение элементов кортежа нельзя изменить потому что кортеж не поддерживает обращение по элементам ''tuple' object does not support item assignment'
+
+mutable_list = (1234,5678)
+mutable_list = (1234,5678*10)
+
+print (mutable_list)
